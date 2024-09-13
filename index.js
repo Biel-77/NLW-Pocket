3c32e2ccd2a9971c61ef6a355d1e5e0529d15521
Gabriel <brcr873@gmail.com>
2024-09-13 20:24:42 -0300

    corrigindo bug,persistir dados

--- index.js
+++ index.js
@@ -33,6 +33,12 @@ const cadastrarMeta = async () => {
 };
 
 const listarMetas = async () => {
+
+  if(metas,length == 0) {
+    mensagem = "Não existem metas!"
+    return 
+  }
+
     const respostas = await checkbox({
     message: "Use as setas para mudar de metas, o espaço para marcar e desmarcar e o enter para finalizar essa etapa",
     choices: [...metas],
@@ -61,6 +67,12 @@ const listarMetas = async () => {
 };
 
 const metasRealizadas = async () => {
+
+  if(metas,length == 0) {
+    mensagem = "Não existem metas!"
+    return 
+  }
+
   const realizadas = metas.filter((meta) => {
     return meta.checked
   })
@@ -76,6 +88,12 @@ const metasRealizadas = async () => {
 }
 
 const metasAbertas = async () => {
+
+  if(metas,length == 0) {
+    mensagem = "Não existem metas!"
+    return 
+  }
+
   const abertas = metas.filter((meta) =>{
     return meta.checked != true
   })
@@ -92,6 +110,12 @@ const metasAbertas = async () => {
 }
 
 const deletarMetas = async () => {
+
+  if(metas,length == 0) {
+    mensagem = "Não existem metas!"
+    return 
+  }
+  
   const metasDesmarcadas = metas.map((meta) => {
     return { value: meta.value, checked: false }
   })
