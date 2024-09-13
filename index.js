2fcab33dd61a4c6e07fa83e62aab8dd8f231ae3c
Gabriel <brcr873@gmail.com>
2024-09-13 20:19:37 -0300

    carregar e salvar mensagens

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const { select, input, checkbox } = require("@inquire/prompts");
 
-let mensagem = "Bem-vindp ao app de metas"
+const fs =require("fs").promises
 
-let metas = [ metas ] 
+let mensagem = "Bem-vindo ao App de metas"
+
+let metas 
+
+const carregarMetas = async () => {
+  try{
+    const dados = await fs.readFile("metas.json", "utf-8")
+    metas = JSON.parse(dados)
+  }
+  catch(erro) {
+    metas = []
+  }
+}
+
+const salvarMetas = async () => {
+  await fs.writeFile("metas.json", JSON.stringify(metas, null, 2))
+}
 
 const cadastrarMeta = async () => {
   const meta = await input({ message: "Digite a meta:"});
@@ -110,7 +126,11 @@ const mostrarMensagem = () => {
   }
 }
 const start = async () => {
+  await carregarMetas()
   while (true) {
+    mostrarMensagem()
+    await salvarMetas()
+
     const opcao = await select({
       message: "Menu>",
       choices: [
